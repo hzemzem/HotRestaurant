7207e5bbe7ca52e3e8431dea55635635585c0284
anas tellat <anas tellat>
2017-08-05 11:32:52 -0400

    added raw server.js

---
+++ server.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var tables = [{
+	name: "Richard",
+	phone: "770-777-9999",
+	email: "richard@email.com",
+	uniqueId: "The Phoenix"
+},
+{
+	name: "Brice",
+	phone: "770-999-7777",
+	email: "brice@email.com",
+	uniqueId: "Bricen"
+},
+{
+	name: "Hajar",
+	phone: "404-hah-ahah",
+	email: "gigi@email.com",
+	uniqueId: "Gigi"
+}];
+
+var waitlist = [];
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.get("/view", function(req, res) {
+  res.sendFile(path.join(__dirname, "view.html"));
+});
+
+// Display all reserved tables
+app.get("/api/:tables?", function(req, res) {
+  var chosen = req.params.tables;
+
+  if (chosen) {
+    console.log(chosen);
+
+    for (var i = 0; i < tables.length; i++) {
+      if (chosen === tables[i].routeName) {
+        return res.json(tables[i]);
+      }
+    }
+
+    return res.json(false);
+  }
+  return res.json(tables);
+});
+
+app.post("/api/new", function(req, res) {
+  var newReservation = req.body;
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  if(tables.length < 5) {
+  	tables.push(newReservation);
+  } 
+  else {
+  	waitlist.push(newReservation);
+  }
+
+  res.json(newReservation);
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
